c0a7e1c91bc2e7b660ac709be134e544a33c7053
Joe Johans <lgvichy@gmail.com>
2017-12-07 15:12:01 -0500

    Adding back the video controller

---
+++ IGME119-2DPlatformer/Assets/Scripts/VideoController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+
+	VideoPlayer movie;
+
+	// Use this for initialization
+	void Start ()
+	{
+		movie = GetComponent<VideoPlayer> ();
+		movie.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!movie.isPlaying) {
+			Debug.Log ("movie end");
+			Application.LoadLevel ("Platformer_Float");
+		}
+	}
+}
