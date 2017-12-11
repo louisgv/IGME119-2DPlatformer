531a9f6ab6d117bb926e9ddf94d69b5264a69da7
Joe Johans <lgvichy@gmail.com>
2017-12-11 14:48:27 -0500

    Added online demo

--- IGME119-2DPlatformer/Assets/Scripts/EnemyScript.cs
+++ IGME119-2DPlatformer/Assets/Scripts/EnemyScript.cs
@@ -2,52 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour {
+public class EnemyScript : MonoBehaviour
+{
 
-    static float platformLength = SpawnPlatforms.horizontalMin;
+	static float platformLength = SpawnPlatforms.horizontalMin;
 
-    [HideInInspector] public bool facingRight = true;
-    [HideInInspector] public bool jump = false;
-    public float moveForce = 365f;
-    public float maxSpeed = 2f;
-    public float jumpForce = 1000f;
-    public Transform groundCheck;
+	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public bool jump = false;
+	public float moveForce = 365f;
+	public float maxSpeed = 2f;
+	public float jumpForce = 1000f;
+	public Transform groundCheck;
 	public Transform groundCheckLeft;
 	public Transform groundCheckRight;
-    public Transform player;
+	public Transform player;
 	public WeaponScript weapon;
 
-    private bool grounded = false;
+	private bool grounded = false;
 	private bool leftGrounded = false;
 	private bool rightGrounded = false;
-    private bool shotLastFrame = false;
+	private bool shotLastFrame = false;
 
-    private Animator anim;
-    private Rigidbody2D rb2d;
+	private Animator anim;
+	private Rigidbody2D rb2d;
 
-    // Use this for initialization
-    void Start () {
-        weapon = this.GetComponentInChildren<WeaponScript> ();
+	// Use this for initialization
+	void Start ()
+	{
+		weapon = this.GetComponentInChildren<WeaponScript> ();
 		weapon.enabled = true;
-        anim = gameObject.GetComponent<Animator>();
-        rb2d = GetComponent<Rigidbody2D>();
-    }
+		anim = gameObject.GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
 	
 	// Update is called once per frame
-	void Update () {
-        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-		leftGrounded = Physics2D.Linecast(transform.position, groundCheckLeft.position, 1 << LayerMask.NameToLayer("Ground"));
-		rightGrounded = Physics2D.Linecast(transform.position, groundCheckRight.position, 1 << LayerMask.NameToLayer("Ground"));
+	void Update ()
+	{
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+		leftGrounded = Physics2D.Linecast (transform.position, groundCheckLeft.position, 1 << LayerMask.NameToLayer ("Ground"));
+		rightGrounded = Physics2D.Linecast (transform.position, groundCheckRight.position, 1 << LayerMask.NameToLayer ("Ground"));
 
 
 		// Auto-Fire
-        if (weapon != null && weapon.enabled && weapon.CanAttack && !anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Shoot"))
-		{
-            //source.PlayOneShot(audio_Shot);
-            Debug.Log(weapon + "Fire");
-            weapon.Attack(true);
-            gameObject.GetComponent<Animator>().SetTrigger("Shoot");
-        }
+		if (weapon != null && weapon.enabled && weapon.CanAttack && !anim.GetCurrentAnimatorStateInfo (0).IsName ("Player_Shoot")) {
+			//source.PlayOneShot(audio_Shot);
+//            Debug.Log(weapon + "Fire");
+			weapon.Attack (true);
+			gameObject.GetComponent<Animator> ().SetTrigger ("Shoot");
+		}
 
 //		// 4 - Out of the camera ? Destroy the game object.
 //		if (GetComponent<Renderer>().IsVisibleFrom(Camera.main) == false)
@@ -58,16 +60,16 @@ public class EnemyScript : MonoBehaviour {
 		if (player != null) {
 			if (player.position.x < gameObject.transform.position.x && leftGrounded) {
 				gameObject.transform.Translate (maxSpeed * Time.deltaTime * -1f, 0, 0);
-                gameObject.GetComponent<Animator>().SetBool ("Moving", true);
+				gameObject.GetComponent<Animator> ().SetBool ("Moving", true);
 			} else if (player.position.x > gameObject.transform.position.x && rightGrounded) {
 				gameObject.transform.Translate (maxSpeed * Time.deltaTime, 0, 0);
-                gameObject.GetComponent<Animator>().SetBool ("Moving", true);
+				gameObject.GetComponent<Animator> ().SetBool ("Moving", true);
 			}
 		}
-    }
+	}
 
-    void FixedUpdate()
-    {
+	void FixedUpdate ()
+	{
 
 
 
@@ -76,34 +78,34 @@ public class EnemyScript : MonoBehaviour {
 //        else if (h < 0 && facingRight)
 //            Flip();
 
-        //if (jump)
-        //{
-        //    //anim.SetTrigger("Jump");
-        //    rb2d.AddForce(new Vector2(0f, jumpForce));
-        //    jump = false;
-        //}
-    }
+		//if (jump)
+		//{
+		//    //anim.SetTrigger("Jump");
+		//    rb2d.AddForce(new Vector2(0f, jumpForce));
+		//    jump = false;
+		//}
+	}
 
 
-    void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+	void Flip ()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 
-	private void endShoot()
+	private void endShoot ()
 	{
 		//anim.SetBool("Shoot",false);
 	}
 
-	private void endWalk()
+	private void endWalk ()
 	{
-        //gameObject.GetComponent<Animator>().SetBool("Moving",false);
+		//gameObject.GetComponent<Animator>().SetBool("Moving",false);
 	}
 
-	private void endInjury()
+	private void endInjury ()
 	{
 		//anim.SetBool("Injury",false);
 	}
